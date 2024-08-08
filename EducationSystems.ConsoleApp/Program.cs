9f3ac18328577957527fccda275fda7051b389b8
mohammadrezaamiri <mohammadreza.amiri.it@gmail.com>
2024-08-08 15:16:34 +0330

    refactor directory of projects

--- EducationSystems.ConsoleApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using EducationSystems;
-
-Console.WriteLine("Hello, World!");
-
-var educationSystem = new EducationSystem();
-educationSystem.AddStudent("ali");
-educationSystem.Archive.ShowStudents();
